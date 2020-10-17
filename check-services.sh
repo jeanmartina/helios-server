1c8843a6b7800cadd3a77873319c481759b34436
Jean Martina <jean.martina@gmail.com>
2020-10-17 16:59:57 +0200

    Changed Checkservices inorder to work with UFSCs automated install script

--- check-services.sh
+++ check-services.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-SERVICES=('celeryd' 'celerybeat')
+#SERVICES=('celeryd' 'celerybeat')
+SERVICES=('celery')
   
 for service in ${SERVICES[@]}; do
     if ps ax | grep -v grep | grep $service > /dev/null
     then
-        echo "$service service running, everything is fine"
+        echo "$service service running, everything is fine" >/dev/null
     else
         echo "$service is not running"
-        cd /var/www/helios-server && \
-        source venv/bin/activate
+        cd /home/votacao/helios-server    
+         
 
-        if [ "$service" == "celeryd" ]; then
-            python manage.py celeryd --events --loglevel=INFO --concurrency=5 -f celery.log &
+        if [ "$service" == "celery" ]; then
+	    /usr/local/bin/celery -A helios worker -l info --concurrency=5 -f celery.log &
         else
             python manage.py $service --logleve=INFO -f $service.log &
         fi
